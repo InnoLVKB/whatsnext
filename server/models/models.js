276b481948fd17b2e3a62519eaa46aaf49d9678a
Krisette Odegard <kmodeg@gmail.com>
2023-02-09 09:26:36 -0800

    chore: removed old db secret

--- server/models/models.js
+++ server/models/models.js
@@ -1,38 +1,35 @@
-// const path = require('path');
-import path from "path";
-// import from 'dotenv'.config()
-// require("dotenv").config({
-//   path: path.resolve(__dirname, './.env')
-// });
+// // const path = require('path');
+// import path from "path";
+// // import from 'dotenv'.config()
+// // require("dotenv").config({
+// //   path: path.resolve(__dirname, './.env')
+// // });
 
-const connectionString =
-  "postgres://pxcddmbe:soXd8-HF0Fz9__fNMoqeFXTq3upUEnWN@heffalump.db.elephantsql.com/pxcddmbe";
+// // const { Pool } = require("pg");
+// // const pool = new Pool({
+// //   connectionString: connectionString,
+// // });
 
-// const { Pool } = require("pg");
+// // module.exports = {
+// //   query: (text, params, callback) => {
+// //     console.log("executed query:", text);
+// //     return pool.query(text, params, callback);
+// //   },
+// // };
+
+// import { Pool } from "pg";
 // const pool = new Pool({
 //   connectionString: connectionString,
 // });
 
-// module.exports = {
-//   query: (text, params, callback) => {
-//     console.log("executed query:", text);
-//     return pool.query(text, params, callback);
-//   },
-// };
+// export default pool;
+// // export default function query() {
+// //   console.log('hello')
+// //   return "brian"
+// // }
 
-import { Pool } from "pg";
-const pool = new Pool({
-  connectionString: connectionString,
-});
-
-export default pool;
-// export default function query() {
-//   console.log('hello')
-//   return "brian"
-// }
-
-// export default function query(text, params, callback) {
-//   console.log('hello')
-//   console.log("executed query:", text);
-//   return pool.query(text, params, callback);
-// }
+// // export default function query(text, params, callback) {
+// //   console.log('hello')
+// //   console.log("executed query:", text);
+// //   return pool.query(text, params, callback);
+// // }
